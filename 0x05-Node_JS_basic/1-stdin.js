8372055ace2921971a113e6df5043eb482559117
Senu-sesi2 <senu.sesi2@gmail.com>
2024-08-21 02:16:14 +0000

    update files

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,27 +1,15 @@
-// A program that prompts users to input data
-// Accepts input
-// Display the result
-// Exit the program
-
-const readline = require('readline');
-
-// Create an interface for reading input and output via the command line
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+console.log('Welcome to Holberton School, what is your name?');
+// await input
+process.stdin.resume();
+// once input is entered
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
+  // check if input is coming from the terminal
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
-
-// Display the welcome message and prompt the user for their name
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  // Display the user's input
-  console.log(`Your name is: ${name}`);
-
-  rl.close();
-});
-
-// Exit message when closing the program
-rl.on('close', () => {
-  console.log('This important software is now closing');
-});
-
-
